e5b3ec85c0867ff91c364ee4f607f495214bacd0
adetunjii <adetunjithomas1@outlook.com>
2022-04-05 23:54:26 +0100

    update: alternative approach to remove duplicates problem

--- dsa_questions/Arrays/RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray.go
+++ dsa_questions/Arrays/RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray.go
@@ -1,14 +1,26 @@
 package dsaquestions
 
-func RemoveDuplicatesFromSortedArray(nums []int) int {
-	leftPointer := 1
+//func RemoveDuplicatesFromSortedArray(nums []int) int {
+//	leftPointer := 1
+//
+//	for i := 1; i <= len(nums)-1; i++ {
+//		if nums[i] != nums[i-1] {
+//			nums[leftPointer] = nums[i]
+//			leftPointer++
+//		}
+//	}
+//
+//	return leftPointer
+//}
 
-	for i := 1; i <= len(nums)-1; i++ {
-		if nums[i] != nums[i-1] {
-			nums[leftPointer] = nums[i]
-			leftPointer++
+func RemoveDuplicatesFromSortedArray(nums []int) int {
+	l := 0
+	for i := 0; i < len(nums); i++ {
+		if i == 0 || nums[i] != nums[i-1] {
+			nums[l] = nums[i]
+			l++
 		}
 	}
 
-	return leftPointer
+	return l
 }
