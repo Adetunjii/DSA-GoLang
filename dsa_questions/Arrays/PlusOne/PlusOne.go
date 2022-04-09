5caf4df23b5f9804103737da185a0439773e310a
adetunjii <adetunjithomas1@outlook.com>
2022-04-09 16:56:40 +0100

    new: Plus One((easy)

---
+++ dsa_questions/Arrays/PlusOne/PlusOne.go
@@ -0,0 +1,18 @@
+package PlusOne
+
+func PlusOne(digits []int) []int {
+
+	right := len(digits) - 1
+	carry := 1
+
+	for i := right; i >= 0; i-- {
+		sum := digits[i] + carry
+		digits[i] = sum % 10
+		carry = sum / 10
+	}
+
+	if carry > 0 {
+		return append([]int{carry}, digits...)
+	}
+	return digits
+}
---
+++ dsa_questions/Arrays/PlusOne/PlusOne_test.go
@@ -0,0 +1,14 @@
+package PlusOne
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestPlusOne(t *testing.T) {
+	input := []int{9, 9, 9}
+	expected := []int{1, 0, 0, 0}
+	output := PlusOne(input)
+
+	require.Equal(t, expected, output)
+}
