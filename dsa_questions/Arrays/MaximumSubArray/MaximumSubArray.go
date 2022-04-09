2c1b529e5923318493a2fb662afcef1622418abc
adetunjii <adetunjithomas1@outlook.com>
2022-04-09 14:10:31 +0100

    update: Maximum SubArray

--- dsa_questions/Arrays/MaximumSubArray/MaximumSubArray.go
+++ dsa_questions/Arrays/MaximumSubArray/MaximumSubArray.go
@@ -1,9 +1,67 @@
 package MaximumSubArray
 
-import "math"
+// MaximumSubArray
+/* Suboptimal approach
+ * Time Complexity O(N ^ 3), Space Complexity O(1)
+ * Literally loop through the array and get the sum of each sub array
+ */
+//func MaximumSubArray(array []int) int {
+//
+//	maxSum := math.MinInt32
+//
+//	for i := 0; i < len(array); i++ {
+//		for j := i + 1; j < len(array); j++ {
+//			windowSum := 0
+//			for k := i; k < j; k++ {
+//				windowSum += array[k]
+//			}
+//			maxSum = max(maxSum, windowSum)
+//		}
+//	}
+//
+//	return maxSum
+//}
 
+// MaximumSubArray
+/* Suboptimal approach
+ * Time Complexity O(N ^ 2), Space Complexity O(1)
+ */
+//func MaximumSubArray(array []int) int {
+//	maxSum := math.MinInt32
+//
+//	for i := 0; i < len(array)-1; i++ {
+//		currentSum := 0
+//
+//		for j := i; j < len(array); j++ {
+//
+//			currentSum += array[j]
+//			maxSum = max(maxSum, currentSum)
+//		}
+//	}
+//
+//	return maxSum
+//}
+
+// MaximumSubArray
+/* Optimal Approach
+ * Time Complexity O(N), Space Complexity O(1)
+ */
 func MaximumSubArray(array []int) int {
-	currentMax := math.MinInt32
+	maxSum := array[0]
+	currentSum := array[0]
 
+	for i := 1; i < len(array); i++ {
+		currentSum = max(array[i], currentSum+array[i])
+		maxSum = max(currentSum, maxSum)
+	}
 
+	return maxSum
+}
+
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+
+	return a
 }
--- dsa_questions/Arrays/MaximumSubArray/MaximumSubArray_test.go
+++ dsa_questions/Arrays/MaximumSubArray/MaximumSubArray_test.go
@@ -1,10 +1,15 @@
 package MaximumSubArray
 
-import "testing"
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
 
 func TestMaximumSubArray(t *testing.T) {
 
-	input := []int{-2,1,-3,4,-1,2,1,-5,4}
+	input := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
 	expected := 6
 	output := MaximumSubArray(input)
+
+	require.Equal(t, expected, output)
 }
