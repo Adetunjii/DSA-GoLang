f087f61da3552f7716394063cc4713d5737e3a5e
adetunjii <adetunjithomas1@outlook.com>
2022-05-10 10:40:36 +0100

    new: Group Anagrams(medium)

---
+++ dsa_questions/Arrays/GroupAnagrams/GroupAnagrams.go
@@ -0,0 +1,33 @@
+package GroupAnagrams
+
+import "sort"
+
+// Time O(s * wlogw) Space => O(sw)
+func GroupAnagrams(strs []string) [][]string {
+	hashMap := make(map[string][]string)
+	var result [][]string
+
+	if len(strs) == 0 {
+		return result
+	}
+
+	for i := 0; i < len(strs); i++ {
+		sortedWord := sortWord(strs[i])
+		hashMap[sortedWord] = append(hashMap[sortedWord], strs[i])
+	}
+
+	for _, value := range hashMap {
+		result = append(result, value)
+	}
+
+	return result
+}
+
+func sortWord(word string) string {
+	wordBytes := []byte(word)
+	sort.Slice(wordBytes, func(i, j int) bool {
+		return wordBytes[i] < wordBytes[j]
+	})
+
+	return string(wordBytes)
+}
---
+++ dsa_questions/Arrays/GroupAnagrams/GroupAnagrams_test.go
@@ -0,0 +1,14 @@
+package GroupAnagrams
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestGroupAnagrams(t *testing.T) {
+	input := []string{"eat", "tea", "tan", "ate", "nat", "bat"}
+	expected := [][]string{{"eat", "tea", "ate"}, {"tan", "nat"}, {"bat"}}
+	output := GroupAnagrams(input)
+
+	require.Equal(t, expected, output)
+}
